aaca574d193b2067ee52869c512f66533bdbe4c2
tim <1536576466@qq.com>
2025-09-21 02:13:18 +0800

    refactor(vue2-project-example): 移除子项目引用
    移除了 vue2-project-example-2.6 和 vue2-project-example-2.7 目录中的子项目引用，
    这些引用指向相同的提交哈希

---
+++ packages/vue2-project-example-2.6x/README.md
@@ -0,0 +1,24 @@
+# vue2-project-example-2.6
+
+## Project setup
+```
+pnpm install
+```
+
+### Compiles and hot-reloads for development
+```
+pnpm run serve
+```
+
+### Compiles and minifies for production
+```
+pnpm run build
+```
+
+### Lints and fixes files
+```
+pnpm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ packages/vue2-project-example-2.6x/src/App.vue
@@ -0,0 +1,67 @@
+<script>
+import TimeScheduleAlone from '../../../simple-vue-time-schedule/TimeScheduleAlone.vue'
+
+export default {
+  components: {
+    TimeScheduleAlone
+  },
+  data() {
+    return {
+      text: '08:00~12:00',
+      timeRange: ['08:00~12:00'],
+      disableRange: ["09:00~11:00", "14:00~17:00"]
+    }
+  },
+  methods: {
+    handlerInputChange(val) {
+      console.log("🚀 ~ handlerInputChange ~ val:", val.target.value)
+      this.timeRange = [val.target.value]
+    },
+    handlerRangeChange(range) {
+      if (range && range.length) {
+        this.text = range[0]
+      } else {
+        this.text = ''
+      }
+    }
+  },
+}
+</script>
+
+
+<template>
+  <div>
+    <input type="text" v-model="text" @change="handlerInputChange">
+    <TimeScheduleAlone canOverlap :model-value="timeRange" :disabled-time-range="disableRange"
+      @change="handlerRangeChange" />
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
